500dbaf95edc9c466b4f29d833060d8f81fbe74d
Hùng <108609419+Conciusieuto@users.noreply.github.com>
2024-03-01 23:55:03 +0800

    Update Địt.js

--- plugins/commands/general/Địt.js
+++ plugins/commands/general/Địt.js
@@ -14,7 +14,7 @@ export async function onCall({ message }) {
 
     if (!mentions || !Object.keys(mentions)[0]) return reply("Please tag someone");
 
-    return GET('https://taochap-6377c502025f.herokuapp.com/getlink4/getlink4')
+    return GET('https://taochap-6377c502025f.herokuapp.com/getlink4')
         .then(async res => {
             let mention = Object.keys(mentions)[0],
                 tag = mentions[mention].replace("@", "");
