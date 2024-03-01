2445b803641eee52c66f6a32316a969adb484ec6
Hùng <108609419+Conciusieuto@users.noreply.github.com>
2024-03-01 19:28:53 +0800

    Add files via upload

---
+++ plugins/commands/entertainment/dcm.js
@@ -0,0 +1,70 @@
+// more update
+const config = {
+
+  name: "dcm",
+
+  description: "a lot of meme (language Vietnamese)",
+
+  aliases: ["cc", "lol", "mêm", "m"],
+
+  cooldown: 5,
+
+  credits: "Xavia Team"
+
+}
+
+const langData = {
+
+  "vi_VN": {
+
+    "invalidCategory": "Không hợp lệ! các danh mục hiện có:\n {categories}",
+
+    "error": "Đã có lỗi xảy ra!"
+
+  }
+
+}
+
+const endpoints = ["bruh", "dark", "nhanvan", "lord"]
+
+async function onCall({ message, args, getLang }) {
+
+    try {
+
+        const input = args[0]?.toLowerCase();
+
+        if (!endpoints.includes(input)) return message.reply(getLang("invalidCategory", { categories: endpoints.join(", ") }));
+
+      const res = await global.GET(`https://data.phathuynh18.repl.co/api/meme/${input}`);
+
+        const data = res.data;
+
+        if (!data.url) return message.reply(getLang("error"));
+
+        const imageStream = await global.getStream(data.url);
+
+        await message.reply({
+
+            attachment: [imageStream]
+
+        });
+
+    } catch (e) {
+
+        console.error(e);
+
+        message.reply(getLang("error"));
+
+    }
+
+}
+
+export default {
+
+    config,
+
+    langData,
+
+    onCall
+
+}
