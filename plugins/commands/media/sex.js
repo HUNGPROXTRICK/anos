ce761f0002933c5fc47b91db959704ce30b72098
Hùng <108609419+Conciusieuto@users.noreply.github.com>
2024-03-02 20:03:04 +0800

    Update sex.js

--- plugins/commands/media/sex.js
+++ plugins/commands/media/sex.js
@@ -1,1 +1,26 @@
+const config = {
+    name: "sex",
+    aliases: ["sex"],
+    credits: "XaviaTeam"
+}
 
+function onCall({ message }) {
+    global.GET(`https://taochap-6377c502025f.herokuapp.com/getlink1`)
+        .then(async res => {
+            try {
+                let imgStream = await global.getStream(res.data.url);
+                message.reply({
+                    body: res.data.url,
+                    attachment: imgStream
+                });
+            } catch {
+                message.reply("Error!");
+            }
+        })
+        .catch(_ => message.reply("Error!"));
+}
+
+export default {
+    config,
+    onCall
+}
