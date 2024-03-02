981122eabdb9aad1a0fca4138c4091f40595fd24
Hùng <108609419+Conciusieuto@users.noreply.github.com>
2024-03-02 12:36:22 +0800

    Update sicbo.js

--- plugins/commands/economy/sicbo.js
+++ plugins/commands/economy/sicbo.js
@@ -1,100 +1,44 @@
 const config = {
-    name: "sicbo",
-    _name: {
-        "vi_VN": "taixiu"
-    },
-    aliases: ["taixiu", "tx"],
-    description: "Play sicbo with bot.",
-    usage: "[tai/xiu] [bet] | [big/small] [bet] (default bet is 50)",
-    cooldown: 5,
-    credits: "XaviaTeam",
+  name: "taixiuuuuuuuuuuuuuuu",
+  description: "",
+  usage: "",
+  cooldown: 3,
   permissions: [2],
-    extra: {
-        minbet: 50
-    }
+};
+
+function onLoad() {
+  if (!global.taixiu_888) {
+    global.taixiu_888 = new Map();
+  }
 }
 
-const langData = {
-    "en_US": {
-        "sicbo.userNoData": "Your data is not ready yet.",
-        "sicbo.invalidChoice": "Invalid choice, available choices:\n{small}\n{big}",
-        "sicbo.notEnoughMoney": "Not enough money.",
-        "sicbo.minMoney": "Minimum bet is {min} XC.",
-        "sicbo.win": "{dices}\nYou won {money} XC.",
-        "sicbo.lose": "{dices}\nYou lost {money} XC.",
-        "sicbo.result_1": "small",
-        "sicbo.result_2": "big",
-        "sicbo.result_0": "triples",
-        "any.error": "An error has occurred, try again later."
-    },
-    "vi_VN": {
-        "sicbo.userNoData": "Dữ liệu của bạn chưa sẵn sàng.",
-        "sicbo.invalidChoice": "Lựa chọn không hợp lệ, các lựa chọn có sẵn:\n{small}\n{big}",
-        "sicbo.notEnoughMoney": "Không đủ tiền.",
-        "sicbo.minMoney": "Cược tối thiểu là {min} XC.",
-        "sicbo.win": "{dices}\nBạn đã thắng {money} XC.",
-        "sicbo.lose": "{dices}\nBạn đã thua {money} XC.",
-        "sicbo.result_1": "xỉu",
-        "sicbo.result_2": "tài",
-        "sicbo.result_0": "ba nút bằng nhau",
-        "any.error": "Đã xảy ra lỗi, vui lòng thử lại sau."
-    },
-    "ar_SY": {
-        "sicbo.userNoData": "البيانات الخاصة بك ليست جاهزة بعد.",
-        "sicbo.invalidChoice": "خيار غير صالح ، الخيارات المتاحة:\n{small}\n{big}",
-        "sicbo.notEnoughMoney": "مال غير كاف.",
-        "sicbo.minMoney": "الحد الأدنى للرهان هو {min} XC.",
-        "sicbo.win": "{dices}\nلقد فزت {money} XC.",
-        "sicbo.lose": "{dices}\nلقد خسرت {money} XC.",
-        "sicbo.result_1": "صغير",
-        "sicbo.result_2": "كبير",
-        "sicbo.result_0": "ثلاث مرات",
-        "any.error": "حدث خطأ ، حاول مرة أخرى في وقت لاحق."
+async function onCall({ message, args }) {
+  const { threadID, senderID } = message;
+  const { Users } = global.controllers
+
+  let input = args[0];
+
+  switch (input) {
+    case "create": {
+      let taixiu = global.taixiu_888.get(threadID);
+      if (taixiu) return message.send(`Nhóm đã có bàn tài xỉu do ${taixiu.author} tạo!`);
+
+      global.taixiu_888.set(threadID, { author: senderID, status: "pending", players: [] });
+
+      message.send("Đã tạo bàn tài xỉu thành công!\nSố người tham gia hiện tại là 0");
+      break;
     }
-}
 
-const small = ["small", "s", "xỉu", "xiu", "x"];
-const big = ["big", "b", "tài", "tai", "t"];
+    case "join": {
+      let taixiu = global.taixiu_888.get(threadID);
 
-async function onCall({ message, args, extra, getLang }) {
-    const { Users } = global.controllers
-    const choice = args[0];
-    const bet = BigInt(args[1] || extra.minbet);
+      if (!taixiu) return message.send("Nhóm hiện không có bàn tài xỉu nào!");
+      if (taixiu.status == "starting") return message.send("Bạn không thể tham gia cược vì bàn tài xỉu của nhóm hiện tại đang bắt đầu!")
 
-    if (!choice || (!big.includes(choice) && !small.includes(choice)))
-        return message.reply(getLang("sicbo.invalidChoice", { small: small.join(", "), big: big.join(", ") }));
-
-    try {
-        const userMoney = await Users.getMoney(message.senderID) || null;
-        if (userMoney === null) return message.reply(getLang("sicbo.userNoData"));
-        if (BigInt(userMoney) < bet) return message.reply(getLang("sicbo.notEnoughMoney"));
-        if (bet < BigInt(extra.minbet)) return message.reply(getLang("sicbo.minMoney", { min: extra.minbet }));
-
-        await Users.decreaseMoney(message.senderID, bet);
-        const valueIncreaseIfWin = BigInt(bet) * BigInt(2);
-
-        const valueToPass = big.includes(choice) ? "tai" : "xiu";
-        const { dices, results } = (await global.GET(`${global.xva_api.main}/taixiu/${valueToPass}`)).data;
-        if (results === "thắng") await Users.increaseMoney(message.senderID, valueIncreaseIfWin);
-
-        const _dices = dices
-            .replace("3 nút bằng nhau", getLang("sicbo.result_0"))
-            .replace("tài", getLang("sicbo.result_2"))
-            .replace("xỉu", getLang("sicbo.result_1"));
-
-        const _results = results === "thắng" ? "win" : "lose";
-        message.reply(getLang(`sicbo.${_results}`, {
-            dices: _dices,
-            money: String(bet)
-        }));
-    } catch (error) {
-        console.error(error);
-        return message.reply(getLang("any.error"));
-    }
-}
-
-export default {
-    config,
-    langData,
-    onCall
-  } 
+      if (taixiu.players.some(e => e.id == senderID)) {
+        message.send("Bạn đã có trong bàn tài xỉu này!");
+      } else {
+        if (isNaN(args[1])) return message.send("Số tiền cược không hợp lệ!");
+        const userMoney = await Users.getMoney(message.senderID);
+        if (parseInt(args[1]) > userMoney) return message.send(`Bạn không đủ tiền để cược, bạn chỉ có ${userMoney}$`);
+        if (!args[2] || !["tài", "xỉu", "tai", "xiu", "t", "x"].includes(args[2].toLowerCase())) return message.send("Bạn phải chọn tài hoặc xỉu!");
